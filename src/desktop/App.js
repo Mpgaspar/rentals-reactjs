148d23303ef40db475efad826f33515ca8bfa508
Marcelo Gaspar <drmpgaspar@gmail.com>
2020-04-16 21:01:58 +0100

    Create script to change device to mobile and desktop

---
+++ src/desktop/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+
+class AppDesktop extends Component {
+    render() {
+        return (<div>
+        <h1>Desktop Page</h1>
+        <button type="button" className="btn btn-primary">Primary</button>
+        </div>
+        ) 
+    }
+}
+
+export default AppDesktop
--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppDesktop from './desktop/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = document.getElementById('root');
+const rootMobile = document.getElementById('root-mobile');
+const cssFrame = document.getElementById('css-frame');
+
+window.onload = function getDevice() {
+  if(window.innerWidth < 800) {
+    ReactDOM.render(<App />, rootMobile);
+  } else {
+    cssFrame.href = 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'
+    ReactDOM.render(<AppDesktop />, root);
+  }
+  console.log(window.innerWidth);
+}
